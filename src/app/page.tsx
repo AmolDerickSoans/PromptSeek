e53afb8a4b4b18394a74d72b78894812eb1d750d
Amol derick soans <amoldericksoans@gmail.com>
2025-02-26 17:52:26 +0530

    added morphing text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,26 @@ import SearchAndFilters from "@/components/SearchAndFilters"
 import BlogListing from "@/components/BlogListing"
 import { heroCarouselQuery } from "@/lib/sanity/queries"
 import { client } from "@/lib/sanity/client"
-
+import { MorphingText } from "@/components/magicui/morphing-text"
 async function getHeroCarouselPosts() {
   return await client.fetch(heroCarouselQuery)
 }
 export default async function  Home() {
   const posts = await getHeroCarouselPosts()
-
+  const texts = [
+    "Hello",
+    "Morphing",
+    "Text",
+    "Animation",
+    "React",
+    "Component",
+    "Smooth",
+    "Transition",
+    "Engaging",
+  ];
   return (
     <div className="container mx-auto px-4">
+      <MorphingText texts={texts} />
        <HeroCarousel posts={posts} />
       <QuickFilterTiles />
       <SearchAndFilters />
